c75c86c97ef5a6b62f9a6a95089a272f7003db3c
pedrozelada <przs.gideon@gmail.com>
2024-07-08 23:21:13 -0500

    ahahah

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,3 +1,5 @@
+
+// to display current weather
 const temp = document.querySelector('#current-temp');
 const desc = document.querySelector('#description-temp');
 const max = document.querySelector('#max-temp');
@@ -11,12 +13,12 @@ const caption = document.querySelector('figcaption');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-21.53&lon=-64.73&units=metric&appid=7a754ba18991783a33af01dad98985b9';
 
-async function apiFetch() {
+async function apiFetch(url) {
     try {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        displayResults(data); // uncomment when ready
+        displayResults(data); 
       } else {
           throw Error(await response.text());
       }
@@ -26,7 +28,7 @@ async function apiFetch() {
   }
 
   function displayResults(data) {
-    temp.innerHTML = `${data.main.temp}&deg;C`;
+    temp.innerHTML = `<b>${data.main.temp}&deg;C</b>`;
     desc.innerHTML = data.weather[0].description;
     max.innerHTML = `High: ${data.main.temp_max}&deg;`;
     min.innerHTML = `Low: ${data.main.temp_min}&deg;`;
@@ -39,27 +41,57 @@ async function apiFetch() {
     icon.setAttribute('alt', data.weather[0].main);
     
   }
-  apiFetch();
-
-
-
+  apiFetch(url);
+//convert the unixformat into time
   function formatTimeFromUnix(unixTime) {
-    // Crear un objeto Date basado en el tiempo Unix (en milisegundos)
     const date = new Date(unixTime * 1000);
-
-    // Obtener horas y minutos en formato de 12 horas
     let hours = date.getHours();
-    const minutes = ('0' + date.getMinutes()).slice(-2); // Asegura que los minutos siempre tengan dos dígitos
-
-    // Determinar si es AM o PM
+    const minutes = ('0' + date.getMinutes()).slice(-2); 
     const ampm = hours >= 12 ? 'pm' : 'am';
-
-    // Convertir horas de 24 a 12 horas
     hours = hours % 12;
-    hours = hours ? hours : 12; // Si hours es 0, se convierte a 12
-
-    // Construir la cadena de tiempo en formato deseado
+    hours = hours ? hours : 12;
     const formattedTime = hours + ':' + minutes + ' ' + ampm;
-
     return formattedTime;
 }
+
+// to display 3 days
+const tod = document.querySelector('#todayc');
+const tom = document.querySelector('#tomorrowc');
+const atom = document.querySelector('#atomorrowc');
+
+const url2 = 'https://api.openweathermap.org/data/2.5/forecast?lat=-21.53&lon=-64.73&units=metric&appid=7a754ba18991783a33af01dad98985b9';
+
+//get days
+let todaydate = new Date();
+let tomorrow= new Date();
+tomorrow.setDate(todaydate.getDate() + 1);
+let aftertomorrow = new Date();
+aftertomorrow.setDate(todaydate.getDate() + 2);
+
+let format = { weekday: 'long' };
+let daytomorrow = tomorrow.toLocaleDateString('en-US', format);
+let dayaftertomorrow = aftertomorrow.toLocaleDateString('en-US', format);
+
+async function apiFetch2(url) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults2(data); 
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+function displayResults2(data) {
+  tod.innerHTML = `Today <b>${data.list[0].main.temp}&deg;C</b>`;
+  tom.innerHTML = `${daytomorrow} <b>${data.list[1].main.temp}&deg;C</b>`;
+  atom.innerHTML = `${dayaftertomorrow} <b>${data.list[2].main.temp}&deg;C</b>`;
+}
+apiFetch2(url2);
+
+
